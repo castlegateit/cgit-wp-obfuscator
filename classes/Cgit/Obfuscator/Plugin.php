b817045fff1981b3a3f246c17e7f7c51b792c72d
John Hughes <john@castlegateit.co.uk>
2022-11-22 18:49:03 +0000

    Add ACF support

--- classes/Cgit/Obfuscator/Plugin.php
+++ classes/Cgit/Obfuscator/Plugin.php
@@ -13,16 +13,21 @@ class Plugin
     {
         add_filter('the_content', [$this, 'sanitizeContent']);
         add_filter('get_the_excerpt', [$this, 'sanitizeContent']);
+        add_filter('acf/load_value', [$this, 'sanitizeContent']);
     }
 
     /**
      * Automatically obfuscate email addresses in content
      *
-     * @param string $content
-     * @return string
+     * @param mixed $content
+     * @return mixed
      */
     public function sanitizeContent($content)
     {
+        if (!is_string($content)) {
+            return $content;
+        }
+
         return (new ContentObfuscator($content))->obfuscate();
     }
 }
